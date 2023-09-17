fad32cc98449c6df0bf5dabbbaa83e33387cc8d7
Myron <>
2023-09-17 17:11:52 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+#[macro_use]
+extern crate diesel;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+// We define a custom type for connection pool to use later.
+pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Loading .env into environment variable.
+    dotenv::dotenv().ok();
+
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    // set up database connection pool
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: DbPool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .wrap(middleware::Logger::default())
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
